c992c22ff84de28dffa648ff81819121357bc95e
leander <leanderperez@gmail.com>
2025-02-19 16:38:00 -0400

    add interactive dashboard with AJAX filtering by branches and update graph generation

--- app/views/dashboard.py
+++ app/views/dashboard.py
@@ -1,3 +1,4 @@
+# filepath: /home/leander/dashboard/app/views/dashboard.py
 from django.shortcuts import render
 from app.models import Reporte
 import pandas as pd
@@ -6,27 +7,28 @@ from plotly.offline import plot
 from django.http import JsonResponse
 
 def generar_grafico(queryset):
-    df = pd.DataFrame(list(queryset.values('coordinador', 'precio')))
-    fig = go.Figure(data=[go.Bar(x=df['coordinador'], y=df['precio'])])
-    fig.update_layout(title='Gráfico de Productos por Categoría')
+    df = pd.DataFrame(list(queryset.values('sucursal')))
+    df = df['sucursal'].value_counts()
+    fig = go.Figure(data=[go.Bar(x=df.index, y=df.values)])
+    fig.update_layout(title='Gráfico de Reportes por Sucursal')
     plot_div = plot(fig, output_type='div')
     return plot_div
 
 def index(request):
-    productos = Reporte.objects.all()  # Obtiene todos los productos
-    categorias = Reporte.objects.values_list('categoria', flat=True).distinct()
-    plot_div = generar_grafico(productos)  # Genera el gráfico con todos los productos
+    reportes = Reporte.objects.all()  # Obtiene todos los reportes
+    sucursales = Reporte.objects.values_list('sucursal', flat=True).distinct()
+    plot_div = generar_grafico(reportes)  # Genera el gráfico
     if request.method == 'GET':  # Maneja la solicitud GET inicial
-        return render(request, 'index.html', {'categorias': categorias, 'plot_div': plot_div})
+        return render(request, 'app/dashboard.html', {'sucursales': sucursales, 'plot_div': plot_div})
     elif request.method == 'POST':  # Maneja la solicitud POST de AJAX
         return actualizar_grafico(request)  # Llama a la vista actualizar_grafico
 
 def actualizar_grafico(request):
     if request.method == 'POST':
-        categoria = request.POST.get('coordinador')  # Obtiene la categoría de POST
-        productos = Reporte.objects.all()
-        if categoria:
-            productos = productos.filter(categoria=categoria)
-        plot_div = generar_grafico(productos)
+        sucursales = request.POST.getlist('sucursal[]')  # Obtiene la lista de sucursales seleccionadas
+        reportes = Reporte.objects.all()
+        if sucursales:
+            reportes = reportes.filter(sucursal__in=sucursales)
+        plot_div = generar_grafico(reportes)
         return JsonResponse({'plot_div': plot_div})
-    return JsonResponse({'error': 'No se proporcionó la categoría'})
+    return JsonResponse({'error': 'No se proporcionaron sucursales'})
