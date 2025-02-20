5b5216ce01590a55e6b0eebc66b8c08c0effa21b
leander <leanderperez@gmail.com>
2025-02-20 15:57:37 -0400

    add 'Pendientes' link to navbar, refactor slider script into main script, and update pendientes template structure

--- app/static/app/script.js
+++ app/static/app/script.js
@@ -73,3 +73,22 @@ document.addEventListener('DOMContentLoaded', function() {
       }, false)
     })
   })()
+
+//Script para los pendientes
+var swiper = new Swiper('.blog-slider', {
+  spaceBetween: 30,
+  effect: 'fade',
+  loop: true,
+  mousewheel: {
+    invert: false,
+  },
+  // autoHeight: true,
+  pagination: {
+    el: '.blog-slider__pagination',
+    clickable: true,
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false, // Permite que el autoplay continúe después de la interacción del usuario
+  }
+});
