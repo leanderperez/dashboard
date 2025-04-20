5130a4bab0eb3b7c40544f1e7dc1272aca2cf45b
leander <leanderperez@gmail.com>
2025-04-20 17:06:27 -0400

    Update .gitignore and enhance backup script for PostgreSQL with email notification

--- backup_postgres.sh
+++ backup_postgres.sh
@@ -3,16 +3,21 @@
 # Variables
 DATABASE="forum"
 USUARIO="leander"
-PASSWORD="Forum.2024"
 CORREO="leanderperez@gmail.com"
-FECHA=$(date +"%Y-%m-%d_%H-%M-%S")
+FECHA=$(date +"%Y-%m-%d_%H-%M")
 ARCHIVO_RESPALDO="$DATABASE-$FECHA.sql.gz"
 
+SUBJECT="Respaldo de $DATABASE ($FECHA)"
+BODY="Respaldo de $DATABASE adjunto."
+ARCHIVO="$ARCHIVO_RESPALDO"
+CORREO_DESTINO="$CORREO"
+
 # Realizar respaldo
-pg_dump -h localhost -U $USUARIO -w -Fc $DATABASE | gzip > $ARCHIVO_RESPALDO
+pg_dump -h localhost -U $USUARIO -Fc $DATABASE | gzip > $ARCHIVO_RESPALDO
 
 # Enviar correo
-echo "Respaldo de $DATABASE adjunto." | mail -s "Respaldo de $DATABASE ($FECHA)" -a $ARCHIVO_RESPALDO $CORREO
+# echo "Respaldo de $DATABASE adjunto." | mail -s "Respaldo de $DATABASE ($FECHA)" -a $ARCHIVO_RESPALDO $CORREO
+echo "$BODY" | mutt -s "$SUBJECT" -a "$ARCHIVO" -- "$CORREO_DESTINO"
 
 # Eliminar archivo de respaldo (opcional)
 rm $ARCHIVO_RESPALDO
