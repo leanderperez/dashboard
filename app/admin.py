c8a4b6122c6a90f139f314243c00bd025f8600d9
leander <leanderperez@gmail.com>
2025-04-28 16:17:26 -0400

    Register SolicitudMaterial and DetalleSolicitud models in admin panel

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Reporte, Material
+from .models import Reporte, Material, SolicitudMaterial, DetalleSolicitud
 
 # Register your models here.
 admin.site.register(Reporte)
 admin.site.register(Material)
+admin.site.register(DetalleSolicitud)
