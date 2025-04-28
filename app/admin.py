90526134d9776effe49ea8a8c2a2596eb91622b6
leander <leanderperez@gmail.com>
2025-04-28 16:27:45 -0400

    Register DetalleSolicitud and SolicitudMaterial models in admin panel

--- app/admin.py
+++ app/admin.py
@@ -5,3 +5,4 @@ from .models import Reporte, Material, SolicitudMaterial, DetalleSolicitud
 admin.site.register(Reporte)
 admin.site.register(Material)
 admin.site.register(DetalleSolicitud)
+admin.site.register(SolicitudMaterial)
