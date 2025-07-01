fc4e7b009b712a4b9d654a73408553276e2eec1f
leander <leanderperez@gmail.com>
2025-07-01 10:33:53 -0400

    Register Perfil model in the admin interface and ensure SolicitudMaterial is properly registered

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Reporte, Material, SolicitudMaterial, DetalleSolicitud
+from .models import Reporte, Material, SolicitudMaterial, DetalleSolicitud, Perfil
 
 # Register your models here.
 admin.site.register(Reporte)
 admin.site.register(Material)
 admin.site.register(DetalleSolicitud)
 admin.site.register(SolicitudMaterial)
+admin.site.register(Perfil)
